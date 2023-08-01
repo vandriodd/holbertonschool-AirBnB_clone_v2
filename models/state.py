545d238adb5d6c8008e2ec3a221cddfbaaedaa23
vandriodd <ludasaadi@gmail.com>
2023-08-01 11:32:47 -0700

    Update state for db storage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class"""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
+
+
+@property
+def cities(self):
+    """Getter"""
+    all_cities = []
+    for key, value in self.all().items():
+        key = key.split(".")
+        if key == 'City' and value.state_id == self.id:
+            all_cities.append(value)
+    return all_cities
