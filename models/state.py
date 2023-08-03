073f7ca807323303fd67d2d802d09800e9d89c1c
vandriodd <ludasaadi@gmail.com>
2023-08-03 13:36:27 -0700

    import declarative base from base model, change to do relationship only if dbstorage is used

--- models/state.py
+++ models/state.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
+from os import getenv
 
 
 class State(BaseModel, Base):
     """State class"""
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
-                          cascade='all, delete-orphan')
-
-
-@property
-def cities(self):
-    """Getter"""
-    all_cities = []
-    for key, value in self.all().items():
-        key = key.split(".")
-        if key == 'City' and value.state_id == self.id:
-            all_cities.append(value)
-    return all_cities
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref="state",
+                            cascade="all, delete, delete-orphan")
+    else:
+        name = ""
+        @property
+        def cities(self):
+            """Getter"""
+            all_cities = []
+            for key, value in self.all().items():
+                key = key.split(".")
+                if key == 'City' and value.state_id == self.id:
+                    all_cities.append(value)
+            return all_cities
