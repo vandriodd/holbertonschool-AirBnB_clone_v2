e18262a06aa40d5657079b3fff4903dd216a8cea
vandriodd <ludasaadi@gmail.com>
2023-08-03 13:34:01 -0700

    change way to delete key in dict

--- models/base_model.py
+++ models/base_model.py
@@ -50,7 +50,7 @@ class BaseModel:
         dictionary['updated_at'] = self.updated_at.isoformat()
 
         if hasattr(self, '_sa_instance_state'):
-            dictionary["_sa_instance_state"].delete()
+           del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
