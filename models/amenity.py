e43ff1edfea97e4eaca08fded28695297a5110de
solp22 <sol.puentereal@gmail.com>
2023-08-04 12:55:01 -0700

    working with dbstorage and add relation w place

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.place import place_amenity
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity class"""
+    __tablename__ = 'amenitites'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity)
+    else:    
+        name = ""
