ac54f2f4462db0fbd7dbe1a8dc42ec82ea243a64
vandriodd <ludasaadi@gmail.com>
2023-08-15 10:07:54 -0700

    Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+  4-number_route module
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_display():
+    """Route handler that display a greeting message"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_display():
+    """Route handler that display 'HBNB'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_display(text):
+    """Route handler that display 'C' followed by text variable"""
+    formatted_text = text.replace('_', ' ')
+    return f"C {formatted_text}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_display(text):
+    """Route handler that display 'Python' followed by given text or default"""
+    formatted_text = text.replace('_', ' ')
+    return f"Python {formatted_text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_display(n):
+    """Route handler that displays if given arg is a number"""
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
