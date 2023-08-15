cff9df63e55ff11927276b7fb37c887bfa879901
vandriodd <ludasaadi@gmail.com>
2023-08-15 11:42:37 -0700

    Added close method for JSON deserialization

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -57,3 +57,7 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """Deserializes the JSON file to objects"""
+        self.reload()
