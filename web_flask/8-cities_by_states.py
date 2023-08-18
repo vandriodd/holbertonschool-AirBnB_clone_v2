8ce470ff2b10f38468bbb797e77e4578a316666b
vandriodd <ludasaadi@gmail.com>
2023-08-18 12:17:32 -0700

    Cities by state

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+  8-cities_by_states module
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_connection(e):
+    """ Closes the storage connection """
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def display_states():
+    """ Displays a list of cities by states """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
